9570a8fb83f2a05b745f1a0d54ef855e8e63e695
AhmedLaMBDA <ahmedamermousa@gmail.com>
2024-01-07 18:01:31 +0200

    Add Routing support using Reitit Library with basic use-cases

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -2,12 +2,10 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.util.http-response :as response]
             [muuntaja.middleware :as muuntaja]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+            [reitit.ring :as reitit]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
 
 (defn html-handler [request-map]
   (response/ok
@@ -19,7 +17,29 @@
   (response/ok
     {:result (get-in request [:body-params :id])}))
 
-(def handler json-handler)
+(def routes
+  [["/" {:get html-handler
+         :post html-handler}]
+   ["/echo/:id" {:get
+                 (fn [{{:keys [id]} :path-params}]
+                   (response/ok (str "<p>the value is: " id "</p>")))}]
+   ["/api" {:middleware [muuntaja/wrap-format]}
+    ["/multiply" {:post
+                  (fn [{{:keys [a b]} :body-params}]
+                    (response/ok {:result (* a b)}))}]]])
+
+(def handler
+  (reitit/ring-handler
+    (reitit/router routes)
+    (reitit/routes
+      (reitit/create-resource-handler {:path "/"})
+      (reitit/create-default-handler
+        {:not-found
+         (constantly (response/not-found "404 - Page Not Found"))
+         :method-not-allowed
+         (constantly (response/method-not-allowed "405 - Not allowed"))
+         :not-acceptable
+         (constantly (response/not-acceptable "406 - Not acceptable"))}))))
 
 (defn wrap-nocache [handler]
   (fn [request]
@@ -27,13 +47,10 @@
         handler
         (assoc-in [:headers "Pragma"] "no-cache"))))
 
-
-
 (defn -main []
   (jetty/run-jetty
     (-> #'handler
         wrap-nocache
-        muuntaja/wrap-format
         wrap-reload)
     {:port 3000
      :join? false}))
